84e19e0bb72dec45a56fe5332c9dd7fcd7cd1323
chrischanwoo <chris@kidsnote.com>
2018-10-28 16:46:52 +0900

    예원 + 아영님 코드 추가

--- brgg_vlog.rb
+++ brgg_vlog.rb
@@ -80,13 +80,13 @@ get '/tapmenu' do
 end
 
 #-------------------------------------------
-# 01 Vlog Creation
+# 01 Vlog Creation (남기 + 은총)
 #-------------------------------------------
 
 
 
 #-------------------------------------------
-# 02 Vlog Default View
+# 02 Vlog Default View (지현 + 찬우)
 #-------------------------------------------
 
 post '/vlog' do
@@ -168,6 +168,135 @@ end
 
 
 #-------------------------------------------
-# 03 Vlog History View
+# 03 Vlog History View (예원+아영)
 #-------------------------------------------
 
+get '/creation/date_choice' do
+	t = Date.today
+	y = t.prev_day
+	# date를 세션에 밀어넣기
+	choice_date = t
+	# 유저가 선택한거다 / 오늘을 선택했다고 가정한 상태.
+	session['choice_date'] = choice_date
+	#d = array{t.strftime('%Y-%m-%d'),
+	#			y.strftime('%Y-%m-%d')}
+
+	redirect '/creation/date_exist'
+	erb :datechoice
+end
+
+post '/creation/date_exist' do
+	#d = array{'t.strftime('%Y/%m/%d')', 't.strftime('%Y/%m/%d') - 1.day'}
+	if v.where('log_date' => session['choice_date']).exists?
+		#브이쩜 로그데이터가 초이스데이터와 같은 날짜가 존재하냐
+		#==> 유저가 고른 날짜가 존재하냐
+		redirect '/creation/date_overwrite'
+		# 존재하면 덮어쓸지 물어보는 페이지로 이동하기
+	else
+		redirect '/creation/question'
+	end
+		# 그렇지 않으면 바로 쓸말 있냐 질문 페이지로 이동
+end
+
+	# Vlog.where("log_date" => session['log_date'], "user_id" => session['user_id'])
+
+get '/creation/date_overwrite' do
+	o = array{true,false}
+	# 덮어쓸지 안쓸지 물어보기 덮어쓰면 true, 아니면 false
+	if o.first
+		v.where('log_date' => session['choice_date']).delete
+		redirect '/creation/question'
+	# 덮어쓴다 그러면 기존 브이로그 데이터 지우고
+	# 질문 페이지로 이동
+	else
+		redirect '/Vlog'
+	end
+		#0 이라 하면 다음 단계 아니면 Vlog(캘린더 뷰) 탭으로 이동
+	erb :overwrite
+end
+
+get '/creation/question' do
+	q = array{true,false}
+	if user_question_choice = array.first 
+		redirect '/creation/video'
+	else
+		redirect '/creation/questionlist'
+	end
+	erb :question
+end
+# 질문할 거리가 있냐 없냐 물어보기
+	# true = 질문할 거 있다
+	# else
+		# puts date 
+		# redirect '/creation/video'
+		# 없다 대답하면 질문 던져주고 이동하기
+
+get '/createion/questionlist' do
+	if Questionlist.where('question_date' => Date.today).exists?
+		@questionlist = Questionlist.where('question_date' => Date.today)
+	else
+		@questionlist = Questionlist.shuffle
+	end
+# qustionlist db에서 qustion_date 값이 오늘과 같은것을 불러 오라 
+# 아니면 나머지에서 셔플하라... 나머지...? 값 중에 하나 랜덤으로 보여줘라...? ㅠㅠ
+	redirect '/creation/video'
+	erb :questionlist
+end
+# 질문 리스트를 넣을 디비만들기 / 
+# 디비에 있는 걸 불러와서 
+
+
+get '/creation/video' do
+	v = video
+	created_date_video = Date.today
+	session['created_date_video'] = created_date_video
+	session['video_link'] = video_link
+	session['video_ptime'] = video_ptime
+
+	redirect '/creation/feeling'
+	erb :video
+#비디오 모름.... ㅠㅠㅠㅠ
+#썸네일 어떻게...?
+end
+
+get '/creation/feeling' do
+	feeling = array{a, b, c, d}
+	# abcd안에 이미지 링크
+	feeling_select = array.first
+	# user가 a를 선택했다고 가정...
+	session['feeling_select'] = feeling_select
+
+	redirect '/creation/tag'
+	erb :feeling
+end
+
+# 감정 선택하기 -> 희.노.애.락 이모티콘 네 개 중 유저가 선택하면
+# 감정 세션(?)에 저장
+
+get '/creation/tag' do
+	#v.tag = params['tag']
+	#session['tag'] = tag
+
+	#-----------
+	session['tag'] = tag
+
+	redirect '/creation/complete'
+	erb :tag
+end
+
+# 태그 한 단어로 사용자가 입력하면 마찬가지로 세션(?) 파람스(?)에 저장하기
+
+get '/creation/complete' do
+	v = Vlog.new
+	v.log_date = session['choice_date']
+	v.create_date = session['created_date_video']
+	v.link = session['video_link']
+	v.ptime = session['video_ptime']
+	v.feeling = session['feeling_select']
+	v.tag = session['tag']
+
+#v에 지금까지 세션에 저장되어있던거를 저장하기
+	v.save
+
+	redirect '/Vlog'
+end
