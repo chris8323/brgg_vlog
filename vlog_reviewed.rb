0dc3ec0f8a42ea8cff928aa1cf7ca51196e6b93d
chrishwang <chrishwang90@gmail.com>
2018-11-18 14:25:07 +0900

    sinatra code 수정 - video upload path - vlog list 추가

--- vlog_reviewed.rb
+++ vlog_reviewed.rb
@@ -66,7 +66,8 @@ post '/vlog' do
   
   user = Device.find_by_token(params[:token]).user
   file = params[:file]
-  path = "#{user.nickname}/#{file[:filename]}"
+  #path = "#{user.nickname}/#{file[:filename]}" 
+  path = "video/#{user.id}/#{file[:filename]}" #video upload path 수정
 
   s3 = Aws::S3::Resource.new(region:'ap-northeast-1')
   obj = s3.bucket('bgbgbg-bgbg').object(path)
@@ -83,4 +84,55 @@ post '/vlog' do
       v.string    :thumbnail_link
       v.integer   :video_ptime
 =end
+end
+
+
+#History View에서 호출할 때 사용할 것.
+get '/list_by_filter' do
+  device = Device.find_by_token(params[:token])
+  unless device.nill?
+    user = device.user
+    unless user.nill?
+      
+      v = user.vlogs.where(:log_date => range(params[:filter_to_date],params[:filter_from_date], #문법 맞는지 확인 필요
+                       :feeling => params[:filter_feeling]) # and조건이 아니라 or조건으로 걸어야 함
+      
+      #Pagination / :page 값을 Fuse에서 받아야 함
+      v = paginate(:page => params[:page], :per_page => 30).to_json
+      
+    else   
+      error = {:err_code => '002', 
+        :err_msg => '가입되어있지 않은 User입니다.'}
+      error.to_json   
+    end
+  else
+    error = {:err_code => '003', 
+      :err_msg => '등록되어 있지 않은 Device입니다.'}
+    error.to_json 
+  end
+end
+
+# Calender View에서 호출할 때 사용할 것. 
+# 이를 기준으로 특정 날짜를 클릭했을 때, write로 redirect될 지, detail로 redirect될 지 결정된다.
+get '/list_by_month' do
+  device = Device.find_by_token(params[:token])
+  unless device.nill?
+    user = device.user
+    unless user.nill?
+      # :yesr와 :month는 Fuse에서 User가 선택한 값
+      vlog.where(:user_id => user.id,
+                :log_date.year => params[:yeaer], ### 문법에 맞는가??? 확인 필요...
+                :log_date.month => params[:month], ### 문법에 맞는가??? 확인 필요...
+                ).to_json      
+      
+    else   
+      error = {:err_code => '002', 
+        :err_msg => '가입되어있지 않은 User입니다.'}
+      error.to_json   
+    end
+  else
+    error = {:err_code => '003', 
+      :err_msg => '등록되어 있지 않은 Device입니다.'}
+    error.to_json 
+  end
 end
